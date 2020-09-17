7c1a2764ce3ad15cb2376e7991f16d4c5cce428e
Akshay <52507432+Akshay-Savad@users.noreply.github.com>
2020-09-18 03:09:29 +0530

    Updates

---
+++ precache-manifest.d9f19b73db92c00ceb862e3b23590c4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08669692bc1c49b982f61835632c48b0",
+    "url": "/Ecommerce-website/index.html"
+  },
+  {
+    "revision": "b2c1f8719133d2485966",
+    "url": "/Ecommerce-website/static/css/main.700adeaf.chunk.css"
+  },
+  {
+    "revision": "fba77f5143ec06758331",
+    "url": "/Ecommerce-website/static/js/2.f7799ce9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Ecommerce-website/static/js/2.f7799ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2c1f8719133d2485966",
+    "url": "/Ecommerce-website/static/js/main.212e415d.chunk.js"
+  },
+  {
+    "revision": "0c14f08f2af64fb37b39",
+    "url": "/Ecommerce-website/static/js/runtime-main.1e0dcdd4.js"
+  }
+]);
