0cb5c2ae598b6b6cec9eb57c45896a2a7b35de77
Akshay Savad <akshaysavad@gmail.com>
2023-10-14 18:41:48 +0530

    Updates

---
+++ precache-manifest.879e651f82b2161eee7232bd4d113ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7184a1e92284f9996a394e9b4934c173",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0e4fd515c1f4a1dbbab",
+    "url": "/static/css/main.d9e6c171.chunk.css"
+  },
+  {
+    "revision": "b3c84c11c1de2b0ac284",
+    "url": "/static/js/2.e838b1d7.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.e838b1d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0e4fd515c1f4a1dbbab",
+    "url": "/static/js/main.19ccebe8.chunk.js"
+  },
+  {
+    "revision": "e8d23b230335e593b387",
+    "url": "/static/js/runtime-main.744fb23b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Ecommerce-website/precache-manifest.96c503fc32a7ab7420cd1a5085fb38e8.js"
+  "/precache-manifest.879e651f82b2161eee7232bd4d113ec3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ecommerce-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
